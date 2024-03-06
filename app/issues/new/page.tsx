0ff5974db39b8284bdf84772ef8be16e26d44cd7
Mahid Ahmad <mahid797@hotmail.com>
2024-03-06 11:36:15 -0500

    Day 2

---
+++ app/issues/new/page.tsx
@@ -0,0 +1,18 @@
+'use client';
+
+import { Button, TextField } from '@radix-ui/themes';
+import SimpleMDE from 'react-simplemde-editor';
+import 'easymde/dist/easymde.min.css';
+
+const NewIssuePage = () => {
+	return (
+		<div className="max-w-xl space-y-3">
+			<TextField.Root>
+				<TextField.Input placeholder="Title" />
+			</TextField.Root>
+			<SimpleMDE placeholder="Description" />
+			<Button>Submit New Issue</Button>
+		</div>
+	);
+};
+export default NewIssuePage;
