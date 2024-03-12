2091937160857afd4ddb4de4fdb076c9c846f915
Mahid Ahmad <mahid797@hotmail.com>
2024-03-12 04:45:23 -0400

    Day 5

---
+++ app/issues/[id]/page.tsx
@@ -0,0 +1,32 @@
+import prisma from '@/prisma/client';
+import { Box, Grid } from '@radix-ui/themes';
+import { notFound } from 'next/navigation';
+import EditIssueButton from './EditIssueButton';
+import IssueDetails from './IssueDetails';
+
+interface Props {
+	params: { id: string };
+}
+
+const IssueDetailPage = async ({ params }: Props) => {
+	if (Number.isNaN(parseInt(params.id))) notFound();
+
+	const issue = await prisma.issue.findUnique({
+		where: { id: parseInt(params.id) },
+	});
+
+	if (!issue) notFound();
+
+	return (
+		<Grid columns={{ initial: '1', md: '2' }} gap="5">
+			<Box>
+				<IssueDetails issue={issue} />
+			</Box>
+			<Box>
+				<EditIssueButton issueId={issue.id} />
+			</Box>
+		</Grid>
+	);
+};
+
+export default IssueDetailPage;
--- app/issues/loading.tsx
+++ app/issues/loading.tsx
@@ -1,18 +1,17 @@
-import { IssueStatusBadge, Link } from '@/app/components';
-import prisma from '@/prisma/client';
+import { Skeleton } from '@/app/components';
 import { Table } from '@radix-ui/themes';
 import IssueActions from './IssueActions';
 
-const IssuesPage = async () => {
-	const issues = await prisma.issue.findMany();
+const LoadingIssuesPage = () => {
+	const issues = [1, 2, 3, 4, 5];
 
 	return (
-		<div className="mb-5">
+		<div>
 			<IssueActions />
 			<Table.Root variant="surface">
 				<Table.Header>
 					<Table.Row>
-						<Table.ColumnHeaderCell>Issues</Table.ColumnHeaderCell>
+						<Table.ColumnHeaderCell>Issue</Table.ColumnHeaderCell>
 						<Table.ColumnHeaderCell className="hidden md:table-cell">
 							Status
 						</Table.ColumnHeaderCell>
@@ -23,18 +22,18 @@ const IssuesPage = async () => {
 				</Table.Header>
 				<Table.Body>
 					{issues.map((issue) => (
-						<Table.Row key={issue.id}>
+						<Table.Row key={issue}>
 							<Table.Cell>
-								<Link href={`/issues/${issue.id}`}>{issue.title}</Link>
+								<Skeleton />
 								<div className="block md:hidden">
-									<IssueStatusBadge status={issue.status} />
+									<Skeleton />
 								</div>
 							</Table.Cell>
 							<Table.Cell className="hidden md:table-cell">
-								<IssueStatusBadge status={issue.status} />
+								<Skeleton />
 							</Table.Cell>
 							<Table.Cell className="hidden md:table-cell">
-								{issue.createdAt.toDateString()}
+								<Skeleton />
 							</Table.Cell>
 						</Table.Row>
 					))}
@@ -44,5 +43,4 @@ const IssuesPage = async () => {
 	);
 };
 
-export const dynamic = 'force-dynamic';
-export default IssuesPage;
+export default LoadingIssuesPage;
