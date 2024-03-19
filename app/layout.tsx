cb0f8cdbc940edf3e7e81a05e5923be6299499eb
Mahid Ahmad <mahid797@hotmail.com>
2024-03-19 17:30:20 -0400

    .

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({
 }) {
 	return (
 		<html lang="en">
-			<meta name="viewport" content="width=device-width, initial-scale=0.1" />
+			<meta name="viewport" content="width=1024" />
 			<body className={inter.className}>
 				{/* <NavBar /> */}
 				<main>{children}</main>
